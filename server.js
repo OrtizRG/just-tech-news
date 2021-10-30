70ae5f115f0e9281b0ed27324585e3d5971678c7
Reyna <reyna.ortiz84@gmail.com>
2021-10-30 14:00:54 -0700

    added sequelize npm and .env file

---
+++ server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers/');
+const sequelize = require('./config/connection');
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+app.use(express.static(path.join(_dirname, 'public')));
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
